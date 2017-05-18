58090123764df96e5c68ada874a2a99857f85053
muhammadnizaryp <muhammadnizaryp@gmail.com>
2017-05-18 15:22:45 +0700

    commit kedelapan

--- loglan/urls.py
+++ loglan/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 from .forms import login_form
 from django.contrib.auth.views import login
-from django.contrib.auth import views as auth_views
 
 urlpatterns=[
         url(r'^$', views.home_view, name='home'),
@@ -12,10 +11,6 @@ urlpatterns=[
         url(r'^account/confirmed/$', views.account_confirmed_view, name='account_confirmed'),
         url(r'^account/expired/$', views.account_expired_view, name='account_expired'),
         url(r'^login/$', login, name='login', kwargs={"authentication_form": login_form}),
-        url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-        url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-        url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
-        url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
         url(r'^help/$', views.help_view, name='help'),
         url(r'^ranking/$', views.ranking_view, name='ranking'),
         url(r'^logout/$', views.logout_view, name='logout'),
